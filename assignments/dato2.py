5a50061d6c325650ab470e874e4f17856c59a9a5
D49ll <daniel.stangeland@outlook.com>
2021-08-26 14:08:43 +0200

    dato2.py endringer

---
+++ assignments/dato2.py
@@ -0,0 +1,23 @@
+# Oppgave 3.3 (frivillig)
+
+# Brukerinput
+date1 = input("Oppgi en dato (ddmm): ")
+date2 = input("Oppgi enda en dato (ddmm): ")
+
+# 
+
+
+
+def is_valid(input_date):
+    '''
+    A function that verifies correct date format
+    '''
+    str_len = len(input_date)
+
+    if str_len > 4: #Sjekk lengden er riktig, skal være 4
+        return False
+        
+    
+
+
+    
