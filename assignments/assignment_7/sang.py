7c2d247b8622dce1d5d577aa05fe5563c07059db
Daniel Stangeland <daniel.stangeland@outlook.com>
2021-10-14 23:18:08 +0200

    ferdig med kodedel av innlevering

--- assignments/assignment_7/sang.py
+++ assignments/assignment_7/sang.py
@@ -4,24 +4,47 @@ class Sang:
         self._tittel = tittel
 
     def spill(self):
-        print(f"Spiller {self._tittel} av {self._artist}.")
+        print(f"Spiller nå: \'{self._tittel}\' av {self._artist}.")
 
     def sjekkArtist(self, navn):
+        '''
+        Inn:
+        En streng, navn.
+
+        Ut:
+        Basert på om et eller flere av navnene i strengen navn finnes i
+        instansvariablen _artist, returneres True eller False
+        '''
         navn = navn.lower().split()
         artist = self._artist.lower().split()
 
-        for sjekk_navn in navn:
-            for artist_navn in artist:
-                if sjekk_navn == artist_navn:
+        for n in navn:
+            for a in artist:
+                if n == a:
                     return True
         return False           
 
     def sjekkTittel(self, tittel):
+        '''
+        Inn: 
+        En streng, tittel.
+
+        Ut:        
+        Basert på om oppgitt tittel er den samme som instansvariabelen _tittel, 
+        returneres True eller False. 
+        '''
         if tittel.lower() == self._tittel.lower():
             return True
         return False
 
     def sjekkArtistOgTittel(self, artist, tittel):
+        '''
+        Inn:
+        To strenger, artist og tittel.
+
+        Ut:
+        True eller False basert på utfallet av sjekkTittel og sjekkArtist.
+        '''
         if self.sjekkTittel(tittel) and self.sjekkArtist(artist):
             return True
         return False
--- assignments/assignment_7/spilleliste.py
+++ assignments/assignment_7/spilleliste.py
@@ -6,13 +6,82 @@ class Spilleliste:
         self._navn = listenavn
 
     def lesFraFil(self, filnavn):
+        '''
+        Åpner oppgitt fil og leser inn data om sanger.
+        Oppretter et sang-objekt for hver linje.
+        Legger hvert sang-objekt til en spilleliste
+        '''
+
         for linje in open(filnavn,"r"):
             tittel, artist = linje.strip().split(";")
             self._sanger.append(Sang(artist, tittel))
 
     def leggTilSang(self, nySang):
+        '''
+        Legger til en ny sang i spilleliste
+        '''
+
         self._sanger.append(nySang)
 
     def fjernSang(self, sang):
+        '''
+        Fjerner en sang fra spillelisten
+        '''
+
+        self._sanger.remove(sang)
+    
+    def spillSang(self, sang):
+        '''
+        Spiller av ønsket sang
+        '''
+        sang.spill()
+
+    def spillAlle(self):
+        '''
+        Spiller av alle sanger i spillelisten til objektet
+        '''
+
+        for sang in self._sanger:
+            self.spillSang(sang)
         
-        self._sanger
+        '''
+        For-løkken er over er ekvivalent med løkken under,
+        som er kommentert ut.
+        Velger å bruke førstnevnte da dette er mer pythonic kode
+
+        for i in range(len(self._sanger)):
+            self.spillSang(self._sanger[i])
+        '''
+        
+    def finnSang(self, tittel):
+        '''
+        Inn:
+        En streng, tittel
+
+        Ut:
+        Returnerer et sang-objekt basert på tittel, dersom det finnes i spillelisten
+        Dersom det ikke finnes returneres None
+        '''
+
+        for sang in self._sanger:
+            if sang.sjekkTittel(tittel):
+                return sang
+        return None
+
+    def hentArtistUtvalg(self, artistnavn):
+        '''
+        Inn:
+        En streng, artistnavn
+
+        Ut:
+        En liste, utvalg, som inneholder alle sangene fra spillelisten for
+        oppgitt artist. Skulle artisten ikke finnes i spillelisten returneres
+        en tom liste.
+        '''
+
+        utvalg = []
+        for sang in self._sanger:
+            if sang.sjekkArtist(artistnavn):
+                utvalg.append(sang)
+        
+        return utvalg
